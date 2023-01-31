f8fbf56791d43f1f14aeaec78ff4458bbafa4572
Karlis <karlisstekels.dev@gmail.com>
2023-01-31 13:09:44 +0200

    initial commit

---
+++ Async Await tutorial/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Async Await tutorial
+//
+//  Created by karlis.stekels on 31/01/2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}

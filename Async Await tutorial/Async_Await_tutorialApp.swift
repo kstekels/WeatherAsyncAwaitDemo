ab5e96fd1729bf4ad1c708ff265132806539ff45
Karlis <karlisstekels.dev@gmail.com>
2023-01-31 10:22:57 +0200

    Initial Commit

---
+++ Async Await tutorial/Async_Await_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Await_tutorialApp.swift
+//  Async Await tutorial
+//
+//  Created by karlis.stekels on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Async_Await_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
